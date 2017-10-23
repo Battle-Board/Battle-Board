b1ec0d7c51d5067891dd5ebca6b6284d96693737
Courtney Radford <Courtney6554@Courtneys-MacBook-Pro.local>
2017-10-23 12:02:12 -0700

    Monster

--- client/src/components/Monster/Monster.js
+++ client/src/components/Monster/Monster.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Monster.css"
 
 const Monster = ({ props }) =>
-    <div className="col-sm-4 monCard">
+    <div className="col-sm-4 monsCard">
         <div class="panel panel-default">
             <div class="panel-body">
-                <p className="monaName">{props.monsName}</p>
+                <p className="monsName">{props.monsName}</p>
                 <p className="bodyText"></p>
             </div>
         </div>
