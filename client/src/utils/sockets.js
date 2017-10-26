4e82a9aa5449a158fd4a5898c4cbc3c0ec9cd628
Brian P. Evans <dhwyll@gmail.com>
2017-10-26 10:33:11 -0700

    Prepping for merge

--- client/src/utils/sockets.js
+++ client/src/utils/sockets.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client';
 // comment this out for local
 // const SOCKETS_URL = 'https://create-react-express-sockets.herokuapp.com/'
-const SOCKETS_URL = "http://localhost:3000/"
+const SOCKETS_URL = "http://localhost:3001/"
 // comment this out to work locally
 //const SOCKETS_URL = 'http://localhost:3001';
 const socket = openSocket(SOCKETS_URL);
